88c76ad97d8851129d26878c96faee70723f51e4
Gabriel de Souza Passos <gabrielpassos@ucl.br>
2021-08-23 22:34:55 -0300

    Att

--- app.py
+++ app.py
@@ -7,13 +7,41 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def Pagina():  # put application's code here
+def index():  # put application's code here
     return render_template('PaginaWeb.html')
 
-@app.route('/testando')
-def teste():
+
+@app.route('/user_tweets')
+def user_tweets():
 
     #buscar = request.arg.get("buscar")
+
+    consumer_key = "QO61pvhO0VdFaZBIGJLbhRBfG"
+    consumer_secret = "vuN4ATiwzf3BenkzkeG3kXr6ISGZ17makoOgEuoyt7Rs3JHGel"
+    access_token = "1427399398481399819-hAhAKowuXS7fNFYrSMUpBYiv6Qmm5P"
+    access_token_secret = "NMx85GIh4PzVY54PQIcoYfEFSdgSjfSoy1fSVE45UNbdX"
+
+    auth = tw.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tw.API(auth, wait_on_rate_limit=True)
+
+    #public_tweets = api.home_timeline()
+    public_tweets = api.user_timeline()
+
+    i = 0
+    dicionario = {}
+
+    for tweet in public_tweets:
+        dicionario[i] = tweet.text
+        i+=1
+
+    return json.dumps(dicionario)
+    #return str(tweet._json)
+
+
+@app.route('/timeline_tweets')
+def timeline_tweets():
 
     consumer_key = "QO61pvhO0VdFaZBIGJLbhRBfG"
     consumer_secret = "vuN4ATiwzf3BenkzkeG3kXr6ISGZ17makoOgEuoyt7Rs3JHGel"
@@ -33,10 +61,10 @@ def teste():
 
     for tweet in public_tweets:
         dicionario[i] = tweet.text
-        i+=1
+        i += 1
 
     return json.dumps(dicionario)
-    #return str(tweet._json)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
