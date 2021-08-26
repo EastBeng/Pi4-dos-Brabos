426e64ac4ee66791c9a11efde3f27a1d5e8bea77
Gabriel de Souza Passos <gabrielpassos@ucl.br>
2021-08-26 15:20:40 -0300

    Atualizações

--- app.py
+++ app.py
@@ -10,6 +10,29 @@ app = Flask(__name__)
 def index():  # put application's code here
     return render_template('PaginaWeb.html')
 
+@app.route('/tweets')
+def tweets():
+    consumer_key = "QO61pvhO0VdFaZBIGJLbhRBfG"
+    consumer_secret = "vuN4ATiwzf3BenkzkeG3kXr6ISGZ17makoOgEuoyt7Rs3JHGel"
+    access_token = "1427399398481399819-hAhAKowuXS7fNFYrSMUpBYiv6Qmm5P"
+    access_token_secret = "NMx85GIh4PzVY54PQIcoYfEFSdgSjfSoy1fSVE45UNbdX"
+
+    auth = tw.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tw.API(auth, wait_on_rate_limit=True)
+    public_tweets = api.home_timeline()
+
+    i = 0
+    tweets = {}
+
+    for tweet in public_tweets:
+        tweets[i] = tweet._json
+        i += 1
+
+    #return tweets
+    return render_template("Tweets.html", tweets=tweets)
+
 
 @app.route('/user_tweets')
 def user_tweets():
