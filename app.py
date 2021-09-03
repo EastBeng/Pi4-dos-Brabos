5de89a7908b48613809d9b56f3244a6d46f57878
savifon <savifon1@gmail.com>
2021-09-02 22:30:11 -0300

    definindo layout como index e implementando o form

--- app.py
+++ app.py
@@ -6,12 +6,49 @@ import tweepy as tw
 app = Flask(__name__)
 
 
+#@app.route('/')
+#def index():  # put application's code here
+#    return render_template('PaginaWeb.html')
+
+#@app.route('/tweets')
+#def tweets():
 @app.route('/')
 def index():  # put application's code here
-    return render_template('PaginaWeb.html')
+    consumer_key = "QO61pvhO0VdFaZBIGJLbhRBfG"
+    consumer_secret = "vuN4ATiwzf3BenkzkeG3kXr6ISGZ17makoOgEuoyt7Rs3JHGel"
+    access_token = "1427399398481399819-hAhAKowuXS7fNFYrSMUpBYiv6Qmm5P"
+    access_token_secret = "NMx85GIh4PzVY54PQIcoYfEFSdgSjfSoy1fSVE45UNbdX"
+    auth = tw.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tw.API(auth, wait_on_rate_limit=True)
 
-@app.route('/tweets')
-def tweets():
+    public_tweets = api.home_timeline()
+
+    if request.args.get("busca"):
+        palavra_busca = request.args.get("busca")
+    else:
+        palavra_busca = ""
+
+    i = 0
+    tweets = {}
+
+    for tweet in public_tweets:
+        tweets[i] = tweet._json
+        i += 1
+
+    return render_template("Tweets.html", tweets=tweets, busca=palavra_busca)
+
+@app.route('/buscar_tweets')
+def buscar_tweets():
+
+    buscar = request.args.get("pesquisa")
+    tipo_tweet = request.args.get("tipo_tweet")
+
+    if tipo_tweet=="Meus Tweets":
+        template_lista_tweets = "UserTweets.html"
+    else:
+        template_lista_tweets = "TimeLineTweets.html"
+
     consumer_key = "QO61pvhO0VdFaZBIGJLbhRBfG"
     consumer_secret = "vuN4ATiwzf3BenkzkeG3kXr6ISGZ17makoOgEuoyt7Rs3JHGel"
     access_token = "1427399398481399819-hAhAKowuXS7fNFYrSMUpBYiv6Qmm5P"
@@ -21,18 +58,24 @@ def tweets():
     auth.set_access_token(access_token, access_token_secret)
 
     api = tw.API(auth, wait_on_rate_limit=True)
-    public_tweets = api.home_timeline()
+
+    #public_tweets = api.home_timeline()
+    public_tweets = api.user_timeline()
 
     i = 0
-    tweets = {}
+
+    dicionario = {}
 
     for tweet in public_tweets:
-        tweets[i] = tweet._json
-        i += 1
+        dicionario[i] = tweet.text
+        i+=1
 
-    #return tweets
-    return render_template("Tweets.html", tweets=tweets)
+    #return json.dumps(dicionario)
+    #return str(tweet._json)
 
+    aux = dicionario
+
+    return render_template(template_lista_tweets, timeline=aux, busca=buscar)
 
 @app.route('/user_tweets')
 def user_tweets():
