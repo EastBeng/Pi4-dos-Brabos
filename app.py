2b4abe7eef0fadd27347e6111ebd44dcec471937
CaioRS <69554098+CaioRS@users.noreply.github.com>
2021-11-22 19:15:33 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+from typing import Text
 from flask import Flask, render_template, request
 import tweepy as tw
 from sentilex import Score_sentimento
+import spidev
+import ws2812
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():  # put application's code here
+
+    spi = spidev.SpiDev()
+    spi.open(0,0)
+    ws2812.write2812(spi, [[0,0,0] * 33])
 
     #twitter
 
@@ -27,15 +33,34 @@ def index():  # put application's code here
 
     i = 0
     tweets = {}
-
+    armpol = 0
     for tweet in public_tweets:
         tweets[i] = tweet._json
         tweets[i]["polaridade"] = Score_sentimento(tweets[i]["text"])
+        
+        if(palavra_busca in str(tweets[i]["text"]) and palavra_busca != ""):
+            armpol += int(tweets[i]["polaridade"].split(":")[1])
+        elif(palavra_busca == ""):
+            armpol += int(tweets[i]["polaridade"].split(":")[1])
         i += 1
 
+    media = 0
+    media = armpol / i
     #return tweets
+    if(media > 0):
+        ws2812.write2812(spi, [[10,0,0] * 33])
+    if(media == 0):
+        ws2812.write2812(spi, [[8,9 ,0 ] * 33])
+    if(media < 0):
+        ws2812.write2812(spi, [[0,10,0] * 33])
+    
+    i = 0
+    armpol = 0
+    media = 0
     return render_template("Tweets.html", tweets=tweets, busca=palavra_busca)
 
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=80)
+    app.run(debug=True, port=80, host='0.0.0.0')
